8bdd721764b1b6678b33c82708c691dbe90c3ca5
Jack Shao <jack.shao@qq.com>
2022-12-21 17:09:26 +0800

    集成qr_code_scanner。

--- lib/app/modules/main/main_page.dart
+++ lib/app/modules/main/main_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,20 +12,62 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? _controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+
+    if (Platform.isAndroid) {
+      _controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      _controller!.resumeCamera();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("二维码扫描"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Scanner'),
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: (result != null)
+                  ? Text(
+                      'Barcode Type: ${describeEnum(result!.format)} Data: ${result!.code}')
+                  : const Text('Scan a code'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    _controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+
+    super.dispose();
   }
 }
