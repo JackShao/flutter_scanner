ebb8be6a27b902cef8449c60cac1c48027fab1be
Jack Shao <jack.shao@qq.com>
2022-12-21 23:45:40 +0800

    添加调用系统直接运行。

--- lib/app/modules/result/result_page.dart
+++ lib/app/modules/result/result_page.dart
@@ -1,6 +1,7 @@
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ResultPage extends StatefulWidget {
   final Barcode data;
@@ -41,35 +42,7 @@ class _ResultPageState extends State<ResultPage> {
                   borderRadius: BorderRadius.circular(30.0),
                   color: Colors.blueGrey[100],
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    GestureDetector(
-                      onTap: () => _copyToClipboard(msg),
-                      child: const Icon(
-                        Icons.copy,
-                        color: Colors.blue,
-                        size: 30.0,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: const Icon(
-                        Icons.explore,
-                        color: Colors.blue,
-                        size: 30.0,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: const Icon(
-                        Icons.send,
-                        color: Colors.blue,
-                        size: 30.0,
-                      ),
-                    ),
-                  ],
-                ),
+                child: _buildActionBar(msg),
               ),
             ),
           ],
@@ -78,13 +51,64 @@ class _ResultPageState extends State<ResultPage> {
     );
   }
 
-  _copyToClipboard(String msg) {
-    FlutterClipboard.copy(msg).then((_) {
-      const snackBar = SnackBar(
-        content: Text('拷贝成功'),
-      );
+  Widget _buildActionBar(String msg) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        GestureDetector(
+          onTap: () => _copyToClipboard(msg),
+          child: const Icon(
+            Icons.copy,
+            color: Colors.blue,
+            size: 30.0,
+          ),
+        ),
+        GestureDetector(
+          onTap: () => _urlLauncher(msg),
+          child: const Icon(
+            Icons.explore,
+            color: Colors.blue,
+            size: 30.0,
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Icon(
+            Icons.send,
+            color: Colors.blue,
+            size: 30.0,
+          ),
+        ),
+      ],
+    );
+  }
 
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    });
+  _showSnackBar(String msg) {
+    var snackBar = SnackBar(
+      content: Text(msg),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  /// 复制到剪切板中。
+  ///
+  /// [msg]不能为空串，可以为空格。
+  _copyToClipboard(String msg) {
+    if (msg.isEmpty) return;
+
+    FlutterClipboard.copy(msg).then((_) => _showSnackBar('拷贝成功'));
+  }
+
+  /// 调用系统直接处理[Uri]。
+  _urlLauncher(String msg) async {
+    try {
+      // 解析url，有可能抛出[FormatException]异常。
+      var url = Uri.parse(msg);
+      await launchUrl(url);
+    } on FormatException {
+      _showSnackBar('无法识别');
+      return;
+    }
   }
 }
